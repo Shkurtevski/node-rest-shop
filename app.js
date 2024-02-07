d0ae061ebb4de2d7f5d72de236ff55eb22e0f927
Nenad Shkurtevski <nenad.shkurtevski1@gmail.com>
2024-02-07 21:26:56 +0100

    added error handling

--- app.js
+++ app.js
@@ -13,12 +13,17 @@ app.use("/orders", orderRoutes);
 
 app.use((req, res, next) => {
   const error = new Error("Not Found");
-  error.status(404);
+  error.status = 404;
   next(error);
 });
 
 app.use((error, req, res, next) => {
-  
-})
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 
 module.exports = app;
