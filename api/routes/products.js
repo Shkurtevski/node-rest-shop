3b818d22a670ba095d25e9684a7e6dcfca8156e2
Nenad Shkurtevski <nenad.shkurtevski1@gmail.com>
2024-02-15 17:36:52 +0100

    update document

--- api/routes/products.js
+++ api/routes/products.js
@@ -9,7 +9,11 @@ router.get("/", (req, res, next) => {
     .exec()
     .then((docs) => {
       console.log(docs);
+      // if (docs.length >= 0) {
       res.status(200).json(docs);
+      // } else {
+      //   res.status(404)
+      // }
     })
     .catch((err) => {
       console.log(err);
@@ -63,15 +67,36 @@ router.get("/:productId", (req, res, next) => {
 });
 
 router.patch("/:productId", (req, res, next) => {
-  res.status(200).json({
-    message: "Updated product!",
-  });
+  const id = req.params.productId;
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+  Product.update({ _id: id }, { $set: updateOps })
+    .exec()
+    .then((result) => {
+      console.log(result);
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 });
 
 router.delete("/:productId", (req, res, next) => {
-  res.status(200).json({
-    message: "Deleted product!",
-  });
+  const id = req.params.productId;
+  Product.remove({ _id: id })
+    .exec()
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
 module.exports = router;
