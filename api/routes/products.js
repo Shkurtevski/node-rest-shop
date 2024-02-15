034e3f80d79760408cd7ea9ccb2f1243dd55aadb
Nenad Shkurtevski <nenad.shkurtevski1@gmail.com>
2024-02-15 17:20:18 +0100

    update document

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,9 +5,18 @@ const mongoose = require("mongoose");
 const Product = require("../models/product");
 
 router.get("/", (req, res, next) => {
-  res.status(200).json({
-    message: "Handling GET requests to /products",
-  });
+  Product.find()
+    .exec()
+    .then((docs) => {
+      console.log(docs);
+      res.status(200).json(docs);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
 router.post("/", (req, res, next) => {
@@ -39,7 +48,13 @@ router.get("/:productId", (req, res, next) => {
     .exec()
     .then((doc) => {
       console.log(doc);
-      res.status(200).json(doc);
+      if (doc) {
+        res.status(200).json(doc);
+      } else {
+        res
+          .status(404)
+          .json({ message: "No valid entry found for provided ID" });
+      }
     })
     .catch((err) => {
       console.log("From database", err);
